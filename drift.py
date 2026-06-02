908b63611b87b077dc7dc0e25f05bdd4ee3808b4
Dana Lynn <edutdana@gmail.com>
2026-06-02 14:46:01 -0400

    Add drift analysis functions in drift.py

---
+++ drift.py
@@ -0,0 +1,9 @@
+# Reasoning drift analysis functions
+
+def analyze_drift(text):
+    return {
+        "topic_shift": None,
+        "emotional_drift": None,
+        "logic_drift": None,
+        "stability_score": None
+    }
